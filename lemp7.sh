aa03a1b9296df1cda44c307813293a92b26b0ea9
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-11-26 15:29:19 +0300

    Update lemp7.sh

--- lemp7.sh
+++ lemp7.sh
@@ -17,7 +17,15 @@ sed -i 's/"example.org", "example.ru"//' ~/playbooks/config/main.json
 
 cd ~/playbooks/ && ansible-playbook -e @config/main.json -c local -i inventory_php7 lemp7.yml
 
-wget "https://www.cs-cart.com/index.php?dispatch=pages.get_trial&page_id=297&edition=ultimate" -O /var/www/html/$DOMAIN/cscart.zip
+wget "https://www.cs-cart.com/index.php?dispatch=pages.get_trial&page_id=297&edition=ultimate" -O cscart.zip
+unzip cscart.zip -d /var/www/html/$DOMAIN/
+#cd /var/www/html/$DOMAIN/
+#chown -R service ./
+#chmod 644 config.local.php
+#chmod -R 755 design images var
+#find design -type f -print0 | xargs -0 chmod 644
+#find images -type f -print0 | xargs -0 chmod 644
+#find var -type f -print0 | xargs -0 chmod 644
 
 #phpmyadmin
 apt install phpmyadmin -y
@@ -26,3 +34,5 @@ sed -i -e "s/"example.com"/${DOMAIN}/g" /etc/nginx/sites-enabled/pma.conf
 systemctl restart nginx
 
 #lets
+
+echo "open in web browser    :  http://$DOMAIN
